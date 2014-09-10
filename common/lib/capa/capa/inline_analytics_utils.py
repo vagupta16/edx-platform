9dbce4c9446a02e633302024692406e5f052483c
David Adams <dcadams@stanford.edu>
2014-09-10 16:19:08 -0700

    Different types displayed.

--- common/lib/capa/capa/inline_analytics_utils.py
+++ common/lib/capa/capa/inline_analytics_utils.py
@@ -2,7 +2,7 @@ from lxml import etree
 
 from django.conf import settings
 
-from capa.responsetypes import ChoiceResponse, MultipleChoiceResponse
+from capa.responsetypes import ChoiceResponse, MultipleChoiceResponse, OptionResponse, NumericalResponse, StringResponse, FormulaResponse
 from xmodule.capa_module import CapaModule
 
 def get_responses_data(block):
@@ -13,39 +13,62 @@ def get_responses_data(block):
        - checkboxgroup
        - choicegroup
 
-    Questions with shuffle are not currently supported for in-line analytics.
+    Problems with randomize are not currently supported for in-line analytics.
     If settings.ANALYTICS_ANSWER_DIST_URL is unset then returns None
     """
     responses_data = []
     if settings.ANALYTICS_ANSWER_DIST_URL and isinstance(block, CapaModule):
         responses = block.lcp.responders.values()
         valid_responses = {}
+        rerandomize = False
+        if block.rerandomize != 'never':
+            rerandomize = True
 
         # Each response is an individual question
         for response in responses:
             question_type = None
-            has_shuffle = response.has_shuffle()
+#            has_shuffle = response.has_shuffle()
             if isinstance(response, MultipleChoiceResponse):
                 question_type = 'radio'
             elif isinstance(response, ChoiceResponse):
                 question_type = 'checkbox'
+            elif isinstance(response, OptionResponse):
+                question_type = 'option'
+            elif isinstance(response, NumericalResponse):
+                question_type = 'numerical'
+            elif isinstance(response, StringResponse):
+                question_type = 'string'
+            elif isinstance(response, FormulaResponse):
+                question_type = 'formula'
+            else:
+                question_type == 'other'
 
-            # Only radio and checkbox types are support for in-line analytics at this time
+            # Only radio and checkbox types are supported for in-line analytics graphics at this time
+            # Option, numerical, string and formula are supported for number of students answered and date last updated
             if question_type:
                 # There is only 1 part_id and correct response for each question
                 part_id, correct_response = response.get_answers().items()[0]
-                valid_responses[part_id] = [correct_response, question_type, has_shuffle]
+                valid_responses[part_id] = [correct_response, question_type, ]
 
         if valid_responses:
             part_id = None
+            valid_types = ['checkboxgroup', 'choicegroup', 'optioninput', 'textline', 'formulaequationinput', 'textline']
 
             # Loop through all the nodes finding the group elements for each question
             # We need to do this to get the questions in the same order as on the page
             for node in block.lcp.tree.iter(tag=etree.Element):
                 part_id = node.attrib.get('id', None)
-                if part_id and part_id in list(valid_responses) and node.tag in ['checkboxgroup', 'choicegroup']:
+                if part_id and part_id in list(valid_responses) and node.tag in valid_types:
                     # This is a valid question according to the list of valid responses and we have the group node
-                    # add part_id, correct_response, question_type, has_shufle
-                    responses_data.append([part_id, valid_responses[part_id][0], valid_responses[part_id][1], valid_responses[part_id][2]])
+                    
+                    # Question is not one analytics has data for
+                    if valid_responses[part_id][1] == 'other':
+                        responses_data.append([part_id, None, None, "The analytics cannot be displayed for this type of question."])
+                    # Question (problem actually) has rerandomize != never
+                    elif rerandomize:
+                        responses_data.append([part_id, valid_responses[part_id][0], valid_responses[part_id][1], "The analytics cannot be displayed for this question as it uses randomize."])
+                    # Question is one analytics has data for
+                    else:
+                        responses_data.append([part_id, valid_responses[part_id][0], valid_responses[part_id][1], None])
 
     return responses_data
