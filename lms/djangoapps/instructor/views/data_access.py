c56fff011f77ddffd28caca72142328ab7fbec63
Jim <caijim@github.com>
2014-11-04 16:37:51 -0800

    filtering on courseid and then module key

--- lms/djangoapps/instructor/views/data_access.py
+++ lms/djangoapps/instructor/views/data_access.py
@@ -28,7 +28,7 @@ def get_section_users(course_id, queries):
     for query in queries:
         querySpecific = set()
         #query for people that have interacted with the section
-        qresults = open_query(query)
+        qresults = open_query(course_id, query)
         results.mergeIn(qresults)
     return results
 
@@ -40,20 +40,20 @@ def get_problem_users(course_id, queries):
         querySpecific = set()
         #query for people that have interacted with the section
         if query.filter==PROBLEM_FILTERS.OPENED:
-            qresults = open_query(query)
+            qresults = open_query(course_id, query)
         elif query.filter==PROBLEM_FILTERS.COMPLETED:
-            qresults = completed_query(query)
+            qresults = completed_query(course_id, query)
         if qresults:
             results.mergeIn(qresults)
     return results
 
-def completed_query(query):
-    queryset = StudentModule.objects.filter(module_state_key=query.id).filter(~Q(grade=None))
+def completed_query(course_id, query):
+    queryset = StudentModule.objects.filter(module_state_key=query.id, course_id = course_id).filter(~Q(grade=None))
     return processResults(query, queryset)
 
 
-def open_query(query):
-    queryset = StudentModule.objects.filter(module_state_key=query.id)
+def open_query(course_id, query):
+    queryset = StudentModule.objects.filter(module_state_key=query.id, course_id = course_id)
     return processResults(query, queryset)
 
 
