44e96e65479caa121ea838ec1ce84e7ca0e402ab
stv <stv@stanford.edu>
2015-04-28 15:55:21 -0700

    keyword sub

--- cms/startup.py
+++ cms/startup.py
@@ -23,17 +23,17 @@ def run():
 
     add_mimetypes()
 
-<<<<<<< HEAD
+# TODO:FUNK <<<<<<< HEAD
     # Monkey patch the keyword function map
     if keyword_substitution.keyword_function_map_is_empty():
         keyword_substitution.add_keyword_function_map(get_keyword_function_map())
         # Once keyword function map is set, make update function do nothing
         keyword_substitution.add_keyword_function_map = lambda x: None
-=======
+# TODO:FUNK =======
     if settings.FEATURES.get('USE_CUSTOM_THEME', False):
         enable_theme()
 
->>>>>>> 00b75f0119b981641833240be214ef2076329747
+# TODO:FUNK >>>>>>> 00b75f0119b981641833240be214ef2076329747
 
 def add_mimetypes():
     """
