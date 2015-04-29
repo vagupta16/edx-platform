a62e720485fbc85bfa5487ff398d08c682047cef
stv <stv@stanford.edu>
2015-04-28 20:56:40 -0700

    azim: report ordering

--- lms/djangoapps/instructor_task/models.py
+++ lms/djangoapps/instructor_task/models.py
@@ -335,23 +335,23 @@ class S3ReportStore(ReportStore):
         can be plugged straight into an href
         """
         course_dir = self.key_for(course_id, '')
-<<<<<<< HEAD
-        sorted_link_data = sorted(
-            [
-                # each element in the array is a list with the following format: ('date, 'filename', 'url')
-                (key.key.split('/')[-1].split('_')[-1].split('.csv')[0], key.key.split('/')[-1], key.generate_url(expires_in=300))
-                for key in self.bucket.list(prefix=course_dir.key)
-            ],
-            reverse=True
-        )
-        return [
-            (link_data[1], link_data[2])
-            for link_data in sorted_link_data
-=======
+# TODO:FUNK <<<<<<< HEAD
+#         sorted_link_data = sorted(
+#             [
+#                 # each element in the array is a list with the following format: ('date, 'filename', 'url')
+#                 (key.key.split('/')[-1].split('_')[-1].split('.csv')[0], key.key.split('/')[-1], key.generate_url(expires_in=300))
+#                 for key in self.bucket.list(prefix=course_dir.key)
+#             ],
+#             reverse=True
+#         )
+#         return [
+#             (link_data[1], link_data[2])
+#             for link_data in sorted_link_data
+# TODO:FUNK =======
         return [
             (key.key.split("/")[-1], key.generate_url(expires_in=300))
             for key in sorted(self.bucket.list(prefix=course_dir.key), reverse=True, key=lambda k: k.last_modified)
->>>>>>> 00b75f0119b981641833240be214ef2076329747
+# TODO:FUNK >>>>>>> 00b75f0119b981641833240be214ef2076329747
         ]
 
 
@@ -436,24 +436,24 @@ class LocalFSReportStore(ReportStore):
         course_dir = self.path_to(course_id, '')
         if not os.path.exists(course_dir):
             return []
-<<<<<<< HEAD
-        sorted_link_data = sorted(
-            [
-                # each element in the array is a list with the following format: ('date, 'filename', 'url')
-                (filename.split('_')[-1].split('.csv')[0], filename, ('file://' + urllib.quote(os.path.join(course_dir, filename))))
-                for filename in os.listdir(course_dir)
-            ],
-            reverse=True
-        )
-        return [
-            (link_data[1], link_data[2])
-            for link_data in sorted_link_data
-=======
+# TODO:FUNK <<<<<<< HEAD
+#         sorted_link_data = sorted(
+#             [
+#                 # each element in the array is a list with the following format: ('date, 'filename', 'url')
+#                 (filename.split('_')[-1].split('.csv')[0], filename, ('file://' + urllib.quote(os.path.join(course_dir, filename))))
+#                 for filename in os.listdir(course_dir)
+#             ],
+#             reverse=True
+#         )
+#         return [
+#             (link_data[1], link_data[2])
+#             for link_data in sorted_link_data
+# TODO:FUNK =======
         files = [(filename, os.path.join(course_dir, filename)) for filename in os.listdir(course_dir)]
         files.sort(key=lambda (filename, full_path): os.path.getmtime(full_path), reverse=True)
 
         return [
             (filename, ("file://" + urllib.quote(full_path)))
             for filename, full_path in files
->>>>>>> 00b75f0119b981641833240be214ef2076329747
+# TODO:FUNK >>>>>>> 00b75f0119b981641833240be214ef2076329747
         ]
