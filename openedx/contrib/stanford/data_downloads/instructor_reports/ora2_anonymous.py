68ca4f9dca47d3daeccc0fcfb928d4b1dcb1f676
Albert Liang <albertliangcode@gmail.com>
2015-08-13 11:19:59 -0700

    Move email and anonymous ora2 methods to instructor_reports/

--- openedx/contrib/stanford/data_downloads/instructor_reports/ora2_anonymous.py
+++ openedx/contrib/stanford/data_downloads/instructor_reports/ora2_anonymous.py
@@ -2,3 +2,14 @@
 """
 Generate report of anonymous ORA2 data
 """
+
+
+from __future__ import absolute_import
+from openedx.contrib.stanford.data_downloads.util import collect_ora2_data
+
+
+def collect_anonymous_ora2_data(course_id):
+    """
+    Call collect_ora2_data for anonymized, aggregated ORA2 response data.
+    """
+    return collect_ora2_data(course_id, False)
--- openedx/contrib/stanford/data_downloads/instructor_reports/ora2_email.py
+++ openedx/contrib/stanford/data_downloads/instructor_reports/ora2_email.py
@@ -2,3 +2,14 @@
 """
 Generate report of ORA2 data with email address
 """
+
+
+from __future__ import absolute_import
+from openedx.contrib.stanford.data_downloads.util import collect_ora2_data
+
+
+def collect_email_ora2_data(course_id):
+    """
+    Call collect_ora2_data for aggregated ORA2 response data including users' email addresses
+    """
+    return collect_ora2_data(course_id, True)
