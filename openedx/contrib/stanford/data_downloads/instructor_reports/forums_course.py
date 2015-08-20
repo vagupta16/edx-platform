vagupta16/edx-platform

commit 8a7601ab03745a2be12c091060d837e7d77050fb

Make module methods private

openedx/contrib/stanford/data_downloads/instructor_reports/forums_course.py
@@ -50,7 +50,7 @@ def collect_course_forums_data(course_id):
     return header, parsed_results
 
 
-def generate_course_forums_query(course_id, query_type, parent_id_check=None):
+def _generate_course_forums_query(course_id, query_type, parent_id_check=None):
     """
     We can make one of 3 possible queries: CommentThread, Comment, or Response
     CommentThread is specified by _type
@@ -90,7 +90,7 @@ def generate_course_forums_query(course_id, query_type, parent_id_check=None):
     return query
 
 
-def merge_join_course_forums(threads, responses, comments):
+def _merge_join_course_forums(threads, responses, comments):
     """
     Performs a merge of sorted threads, responses, comments data
     interleaving the results so the final result is in chronological order

lms/djangoapps/instructor/utils.py
@@ -40,9 +40,6 @@ def get_module_for_student(student, usage_key, request=None):
 
 
 from openedx.contrib.stanford.data_downloads.instructor_reports.forums_course import collect_course_forums_data
-from openedx.contrib.stanford.data_downloads.instructor_reports.forums_course import merge_join_course_forums
-from openedx.contrib.stanford.data_downloads.instructor_reports.forums_course import generate_course_forums_query
 from openedx.contrib.stanford.data_downloads.instructor_reports.ora2_anonymous import collect_anonymous_ora2_data
 from openedx.contrib.stanford.data_downloads.instructor_reports.ora2_email import collect_email_ora2_data
 from openedx.contrib.stanford.data_downloads.instructor_reports.forums_students import collect_student_forums_data
-from openedx.contrib.stanford.data_downloads.instructor_reports.forums_students import generate_student_forums_query

openedx/contrib/stanford/data_downloads/instructor_reports/forums_students.py
@@ -35,7 +35,7 @@ def collect_student_forums_data(course_id):
     return header, parsed_results
 
 
-def generate_student_forums_query(course_id):
+def _generate_student_forums_query(course_id):
     """
     generates an aggregate query for student data which can be executed using pymongo
     :param course_id:

commit 781b28cd0891f8b264566d048a086f1ac908c0e1

Fix bug introduced in 8a7601a and missed in 90318eb

openedx/contrib/stanford/data_downloads/instructor_reports/forums_course.py
@@ -34,7 +34,7 @@ def collect_course_forums_data(course_id):
 
     for entry in new_responses:
         entry['_id']['type'] = "Response"
-    results = merge_join_course_forums(new_threads, new_responses, new_comments)
+    results = _merge_join_course_forums(new_threads, new_responses, new_comments)
     parsed_results = [
         [
             "{0}-{1}-{2}".format(result['_id']['year'], result['_id']['month'], result['_id']['day']),