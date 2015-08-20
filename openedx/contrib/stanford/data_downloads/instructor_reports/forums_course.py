vagupta16/edx-platform

commit 8a7601ab03745a2be12c091060d837e7d77050fb

Make module methods private

openedx/contrib/stanford/data_downloads/instructor_reports/forums_course.py
@@ -50,7 +50,7 @@ def collect_course_forums_data(course_id):
     return header, parsed_results
 
 
-def generate_course_forums_query(course_id, query_type, parent_id_check=None):
+def _generate_course_forums_query(course_id, query_type, parent_id_check=None):
     """
     We can make one of 3 possible queries: CommentThread, Comment, or Response
     CommentThread is specified by _type
@@ -90,7 +90,7 @@ def generate_course_forums_query(course_id, query_type, parent_id_check=None):
     return query
 
 
-def merge_join_course_forums(threads, responses, comments):
+def _merge_join_course_forums(threads, responses, comments):
     """
     Performs a merge of sorted threads, responses, comments data
     interleaving the results so the final result is in chronological order

openedx/contrib/stanford/data_downloads/instructor_reports/forums_students.py
@@ -35,7 +35,7 @@ def collect_student_forums_data(course_id):
     return header, parsed_results
 
 
-def generate_student_forums_query(course_id):
+def _generate_student_forums_query(course_id):
     """
     generates an aggregate query for student data which can be executed using pymongo
     :param course_id:

lms/djangoapps/instructor/utils.py
@@ -40,9 +40,6 @@ def get_module_for_student(student, usage_key, request=None):
 
 
 from openedx.contrib.stanford.data_downloads.instructor_reports.forums_course import collect_course_forums_data
-from openedx.contrib.stanford.data_downloads.instructor_reports.forums_course import merge_join_course_forums
-from openedx.contrib.stanford.data_downloads.instructor_reports.forums_course import generate_course_forums_query
 from openedx.contrib.stanford.data_downloads.instructor_reports.ora2_anonymous import collect_anonymous_ora2_data
 from openedx.contrib.stanford.data_downloads.instructor_reports.ora2_email import collect_email_ora2_data
 from openedx.contrib.stanford.data_downloads.instructor_reports.forums_students import collect_student_forums_data
-from openedx.contrib.stanford.data_downloads.instructor_reports.forums_students import generate_student_forums_query

commit 90318eb627dc20e77155f0c5cc4cb36d20d8c8f9

Fix bug introduced in commit 8a7601a

openedx/contrib/stanford/data_downloads/instructor_reports/forums_course.py
@@ -22,9 +22,9 @@ def collect_course_forums_data(course_id):
     try:
         client = MongoClient(get_mongo_connection_string())
         mongodb = client[FORUMS_MONGO_PARAMS['database']]
-        new_threads_query = generate_course_forums_query(course_id, "CommentThread")
-        new_responses_query = generate_course_forums_query(course_id, "Comment", False)
-        new_comments_query = generate_course_forums_query(course_id, "Comment", True)
+        new_threads_query = _generate_course_forums_query(course_id, "CommentThread")
+        new_responses_query = _generate_course_forums_query(course_id, "Comment", False)
+        new_comments_query = _generate_course_forums_query(course_id, "Comment", True)
 
         new_threads = mongodb.contents.aggregate(new_threads_query)['result']
         new_responses = mongodb.contents.aggregate(new_responses_query)['result']

openedx/contrib/stanford/data_downloads/instructor_reports/forums_students.py
@@ -19,7 +19,7 @@ def collect_student_forums_data(course_id):
     try:
         client = MongoClient(get_mongo_connection_string())
         mongodb = client[FORUMS_MONGO_PARAMS['database']]
-        student_forums_query = generate_student_forums_query(course_id)
+        student_forums_query = _generate_student_forums_query(course_id)
         results = mongodb.contents.aggregate(student_forums_query)['result']
     except PyMongoError:
         raise