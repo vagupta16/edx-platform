c3c2a01f2a82ac4899620a22a0e8454ab7d6421e
Albert Liang <albertliangcode@gmail.com>
2015-08-20 11:21:34 -0700

    Move push_student_responses_to_s3 from tasks_helper to student_responses

--- lms/djangoapps/instructor_task/tasks_helper.py
+++ lms/djangoapps/instructor_task/tasks_helper.py
@@ -704,38 +704,7 @@ def upload_grades_csv(_xmodule_instance_args, _entry_id, course_id, _task_input,
     return task_progress.update_task_state(extra_meta=current_step)
 
 
-def push_student_responses_to_s3(_xmodule_instance_args, _entry_id, course_id, _task_input, action_name):
-    """
-    For a given `course_id`, generate a responses CSV file for students that
-    have submitted problem responses, and store using a `ReportStore`. Once
-    created, the files can be accessed by instantiating another `ReportStore` (via
-    `ReportStore.from_config()`) and calling `link_for()` on it. Writes are
-    buffered, so we'll never write part of a CSV file to S3 -- i.e. any files
-    that are visible in ReportStore will be complete ones.
-    """
-    start_time = datetime.now(UTC)
-
-    try:
-        course = get_course_by_id(course_id)
-    except ValueError as e:
-        TASK_LOG.error(e.message)
-        return "failed"
-
-    rows = student_response_rows(course)
-
-    # Generate parts of the file name
-    timestamp_str = start_time.strftime("%Y-%m-%d-%H%M")
-    course_id_prefix = urllib.quote(course_id.to_deprecated_string().replace("/", "_"))
-
-    # Perform the actual upload
-    report_store = ReportStore.from_config()
-    report_store.store_rows(
-        course_id,
-        u"{}_responses_report_{}.csv".format(course_id_prefix, timestamp_str),
-        rows
-    )
-
-    return "succeeded"
+from openedx.contrib.stanford.data_downloads.instructor_reports.student_responses import push_student_responses_to_s3
 
 
 def push_course_forums_data_to_s3(_xmodule_instance_args, _entry_id, course_id, _task_input, action_name):
