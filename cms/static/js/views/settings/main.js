875b7e5493e0bf083dceed00760170337e436b26
Kevin Luo <kevkluo@gmail.com>
2016-03-09 16:39:40 -0800

    Resolve cms/static/js/views/settings/main.js

--- cms/static/js/views/settings/main.js
+++ cms/static/js/views/settings/main.js
@@ -1,16 +1,10 @@
 define(["js/views/validation", "codemirror", "underscore", "jquery", "jquery.ui", "js/utils/date_utils", "js/models/uploads",
     "js/views/uploads", "js/utils/change_on_enter", "js/views/license", "js/models/license",
-<<<<<<< HEAD
-    "js/views/utils/view_utils",
-    "js/views/feedback_notification", "jquery.timepicker", "date"],
-    function(ValidatingView, CodeMirror, _, $, ui, DateUtils, FileUploadModel,
-        FileUploadDialog, TriggerChangeEventOnEnter, LicenseView, LicenseModel, ViewUtils, NotificationView) {
-=======
+    "common/js/components/utils/view_utils",
     "common/js/components/views/feedback_notification", "jquery.timepicker", "date", "gettext"],
        function(ValidatingView, CodeMirror, _, $, ui, DateUtils, FileUploadModel,
-                FileUploadDialog, TriggerChangeEventOnEnter, LicenseView, LicenseModel, NotificationView,
+                FileUploadDialog, TriggerChangeEventOnEnter, LicenseView, LicenseModel, ViewUtils, NotificationView,
                 timepicker, date, gettext) {
->>>>>>> hotfix-2015-11-10
 
 var DetailsView = ValidatingView.extend({
     // Model class is CMS.Models.Settings.CourseDetails
