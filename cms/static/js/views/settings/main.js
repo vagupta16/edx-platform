ba4ff78517b18a7b6c0750b36fa755b10c79ff76
Kevin Luo <kevkluo@gmail.com>
2016-03-16 18:01:11 -0700

    Minor format changes to match upstream

--- cms/static/js/views/settings/main.js
+++ cms/static/js/views/settings/main.js
@@ -1,6 +1,6 @@
 define(["js/views/validation", "codemirror", "underscore", "jquery", "jquery.ui", "js/utils/date_utils", "js/models/uploads",
     "js/views/uploads", "js/utils/change_on_enter", "js/views/license", "js/models/license",
-	"common/js/components/utils/view_utils",
+    "common/js/components/utils/view_utils",
     "common/js/components/views/feedback_notification", "jquery.timepicker", "date", "gettext"],
        function(ValidatingView, CodeMirror, _, $, ui, DateUtils, FileUploadModel,
                 FileUploadDialog, TriggerChangeEventOnEnter, LicenseView, LicenseModel, ViewUtils, NotificationView,
