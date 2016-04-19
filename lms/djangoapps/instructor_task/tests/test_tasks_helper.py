8f0ec9d2ee81eea0339258fe220587458a27a79d
Kevin Luo <kevkluo@gmail.com>
2016-04-19 13:16:52 -0700

    Resolve lms/djangoapps/instructor_task/tests/test_tasks_helper.py

--- lms/djangoapps/instructor_task/tests/test_tasks_helper.py
+++ lms/djangoapps/instructor_task/tests/test_tasks_helper.py
@@ -1884,11 +1884,7 @@ class TestCertificateGeneration(InstructorTaskModuleTestCase):
         current_task.update_state = Mock()
         instructor_task = Mock()
         instructor_task.task_input = json.dumps({'students': None})
-<<<<<<< HEAD
-        with self.assertNumQueries(136):
-=======
-        with self.assertNumQueries(213):
->>>>>>> 270ab7f
+        with self.assertNumQueries(225):
             with patch('instructor_task.tasks_helper._get_current_task') as mock_current_task:
                 mock_current_task.return_value = current_task
                 with patch('capa.xqueue_interface.XQueueInterface.send_to_queue') as mock_queue:
